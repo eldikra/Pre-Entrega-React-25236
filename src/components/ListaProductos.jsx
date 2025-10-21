5047e43ebdaaf36f2dd9b994d0824eccbc02e8e4
Diego Kracoff <eldikra@gmail.com.ar>
2025-10-21 03:37:36 -0300

    Actualizado data.txt y agredado de estilo de ListaProductos

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -1,29 +1,32 @@
 
-import React from "react";
 import { Link } from "react-router-dom";
 import { RiProductHuntLine } from "react-icons/ri";
+import "../Styles/ListaProductos.module.css";
 
 const ListaProductos = ({ productos, agregarAlCarrito }) => {
-
   return (
     <div>
-      <h2>Productos disponibles<RiProductHuntLine /></h2>
-      <ul>
+      <h2>
+        Productos disponibles
+        <RiProductHuntLine />
+      </h2>
+      <div className="productos-grid">
         {productos.map((product) => (
-          <li key={product.id} style={{ marginBottom: "10px" }}>
-            {product.prodName} - ${product.prodPrice}
+          <div className="producto-card" key={product.id}>
+            <div style={{ fontWeight: "bold", marginBottom: "8px" }}>
+              {product.prodName}
+            </div>
+            <div style={{ marginBottom: "8px" }}>${product.prodPrice}</div>
             <button
               onClick={() => agregarAlCarrito(product)}
-              style={{ marginLeft: "10px" }}
+              style={{ marginBottom: "8px" }}
             >
               Agregar
             </button>
-            <Link to={`/ProdDetalles/${product.id}`} style={{ marginLeft: "10px" }}>
-              Detalle
-            </Link>
-          </li>
+            <Link to={`/ProdDetalles/${product.id}`}>Detalle</Link>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
